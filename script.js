a8c4e5e89e9c9966fc07555ac69895067f4caf27
Phonique <41431476+marlybone@users.noreply.github.com>
2022-12-07 20:53:05 +0000

    created a box and started importing weather data into box depending on marker location of google map

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const displayTemp = document.querySelector('.weather--temp')
+
+    const locName = '';
+    const temp = '';
+    const theData = '';
+
+const theTemp = () => `${temp}+'°C'`;
+
 var myLat = '';
 var myLng = '';
+
+function displayVariables() {
+displayTemp.innerHTML = theTemp();
+}
+  
 function initMap() {
 var myLatLng = {lat: 59.3293, lng: 18.0686}
 var mapOptions = {
@@ -24,6 +37,7 @@ google.maps.event.addListener(marker, 'dragend', function(){
   myLng = marker.getPosition().lng();
   console.log(myLat + ' And ' + myLng)
   getWeather()
+
 });
 };
 
@@ -31,13 +45,20 @@ function getWeather() {
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '3dcd448ec1msh2b4f01bc724171fp1ee5bdjsn24e082f33ce9',
-		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+			'X-RapidAPI-Key': '3dcd448ec1msh2b4f01bc724171fp1ee5bdjsn24e082f33ce9',
+		  'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
 	}
 };
 
-fetch(`https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=${myLng}&lat=${myLat}`, options)
+fetch(`https://open-weather13.p.rapidapi.com/city/latlon/${myLat}/${myLng}`, options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(data => {
+    const locName = data.name;
+    const temp = Math.round(data.main.temp - 273.15)
+    const theData = data;
+    console.log(locName);
+    console.log(temp);
+    console.log(theData);
+    displayTemp.innerHTML = `${temp}°C`;
+  })
 }
