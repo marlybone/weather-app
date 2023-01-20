66fd05ee035151dd4be8ff48bcdd0d07ec36a29c
M <41431476+marlybone@users.noreply.github.com>
2023-01-20 09:57:40 +0000

    setting variables from the  data

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const displayTemp = document.querySelector('.weather--temp')
 const displayName = document.querySelector('.location--name')
 const displayCountry = document.querySelector('.country--name')
 const displayDescription = document.querySelector('.icon--description')
+const imgBackground = document.querySelector('.top-right');
 /*Open Weather API KEY*/
 const API = 'e65d266285dc01c2ce570d54145b0c1c';
 let myLat = '';
 let myLng = '';
-let country;
-let locName;
+let city;
 let temp;
-let description;
-let descIcon;
-let dIcon;
+let humid;
+let sunset;
+let sunrise;
+let pressure;
+let country;
+let weatherType;
 
-var background = document.querySelector('.top-right');
-background.classList.add('bg-img');
+const background = document.querySelector('.top-right');
+
 
 
 
@@ -51,20 +54,30 @@ google.maps.event.addListener(marker, 'dragend', function(){
 };
 
 function getWeather(){
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLng}&appid=${API}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLng}&appid=${API}&units=metric`)
 .then(response => response.json())
-.then(data =>  {
-  console.log(data)
-  });   
+.then(data => {
+  displayWeather(data)
+  console.log(data);
+  });
 }
 
-
-function  displayWeather() {
-  
+function  displayWeather(data) {
+  temp = data.main.temp;
+  city = data.name;
+  pressure = data.main.pressure;
+  humid = data.main.humidity;
+  sunset = data.sys.sunset;
+  sunrise = data.sys.sunrise;
+  country = data.sys.country;
+  weatherType = data.weather[0].main;
+  console.log(temp, city)
+  console.log(weatherType, sunrise, sunset, humid)
 }
 
 function weatherBackground(weather) {
   switch(weather) {
     case 'Clear':
+      imgBackground.style.setProperty('background-image', 'url("Weather/image.jpg")');
   }
 }
