0fcc3c697d296475f19822d38c16b6d42ef8083e
marlybone <41431476+marlybone@users.noreply.github.com>
2023-08-10 14:10:40 +0200

    commit

--- netlify/functions/grabapikey.js
+++ netlify/functions/grabapikey.js
@@ -4,22 +4,17 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-exports.handler = async (event, context) => {
-    try {
+app.get('/get-maps-api-key', async (req, res) => {
+  try {
       const apiKey = process.env.API_KEY;
       const weatherKey = process.env.API;
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ apiKey, weatherKey }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'An error occurred' }),
-      };
-    }
-  };
+
+      res.json({ apiKey, weatherKey });
+  } catch (error) {
+      console.error('error', error);
+      res.status(500).json({ error: 'An error occurred' });
+  }
+});
   
 
 export default app;
